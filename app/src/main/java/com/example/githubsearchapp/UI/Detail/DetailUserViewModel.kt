22a4e98e28fad61a53edbd25303120790fcd7705
Salvator Sangi <57354073+salvator627@users.noreply.github.com>
2023-12-18 14:49:00 +0700

    Detail User ViewPager

--- app/src/main/java/com/example/githubsearchapp/UI/Detail/DetailUserViewModel.kt
+++ app/src/main/java/com/example/githubsearchapp/UI/Detail/DetailUserViewModel.kt
@@ -1,4 +1,37 @@
 package com.example.githubsearchapp.UI.Detail
 
-class DetailUserViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.githubsearchapp.Api.RetrofitClient
+import com.example.githubsearchapp.Data.Model.DetailResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class DetailUserViewModel : ViewModel() {
+    val user = MutableLiveData<DetailResponse>()
+
+    fun setUserDetail(username : String){
+        RetrofitClient.apiInstance
+            .getDetailUser(username)
+            .enqueue(object  : Callback<DetailResponse>{
+                override fun onResponse(
+                    call: Call<DetailResponse>,
+                    response: Response<DetailResponse>
+                ) {
+                    if (response.isSuccessful){
+                        user.postValue(response.body())
+                    }
+                }
+
+                override fun onFailure(call: Call<DetailResponse>, t: Throwable) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+    }
+    fun getUserDetail(): LiveData<DetailResponse>{
+        return user
+    }
 }
---
+++ app/src/main/java/com/example/githubsearchapp/UI/Detail/FollowingFragment.kt
@@ -0,0 +1,17 @@
+package com.example.githubsearchapp.UI.Detail
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.githubsearchapp.R
+import com.example.githubsearchapp.databinding.FragmentFollowBinding
+
+class FollowingFragment : Fragment(R.layout.fragment_follow) {
+
+    private var _binding  : FragmentFollowBinding? = null
+    private val binding get() = _binding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentFollowBinding.bind(view)
+    }
+}
