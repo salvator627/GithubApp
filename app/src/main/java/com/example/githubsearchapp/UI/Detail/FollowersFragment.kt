868f63fb21c09f9beab41b7a773253e9669c267c
Salvator Sangi <57354073+salvator627@users.noreply.github.com>
2023-12-19 12:11:28 +0700

    Followers Fragment

--- app/src/main/java/com/example/githubsearchapp/UI/Detail/FollowersFragment.kt
+++ app/src/main/java/com/example/githubsearchapp/UI/Detail/FollowersFragment.kt
@@ -3,15 +3,49 @@ package com.example.githubsearchapp.UI.Detail
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.githubsearchapp.R
+import com.example.githubsearchapp.UI.Main.MainAdapter
 import com.example.githubsearchapp.databinding.FragmentFollowBinding
 
 class FollowersFragment : Fragment(R.layout.fragment_follow) {
 
     private var _binding : FragmentFollowBinding? = null
     private val binding get() = _binding!!
+    private lateinit var followersViewModel: FollowersViewModel
+    private lateinit var adapter : MainAdapter
+    private lateinit var username : String
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val args = arguments
+        username = args?.getString(DetailUserActivity.EXTRA_USERNAME).toString()
         _binding = FragmentFollowBinding.bind(view)
+
+        adapter = MainAdapter()
+        adapter.notifyDataSetChanged()
+
+        binding.apply {
+            rvFollower.setHasFixedSize(true)
+            rvFollower.layoutManager = LinearLayoutManager(activity)
+            rvFollower.adapter = adapter
+        }
+        showLoading(state = true)
+        followersViewModel = ViewModelProvider(this,ViewModelProvider.NewInstanceFactory()).get(FollowersViewModel::class.java)
+        followersViewModel.setListUser(username)
+        followersViewModel.getListFollower().observe(viewLifecycleOwner, {
+            if (it!=null){
+                adapter.setList(it)
+                showLoading(false)
+            }
+        })
+    }
+    private fun showLoading(state:Boolean){
+        if (state){
+            binding.progressbar.visibility = View.VISIBLE
+        }else{
+            binding.progressbar.visibility = View.GONE
+        }
     }
 }
--- app/src/main/java/com/example/githubsearchapp/UI/Detail/FollowersViewModel.kt
+++ app/src/main/java/com/example/githubsearchapp/UI/Detail/FollowersViewModel.kt
@@ -1,10 +1,38 @@
 package com.example.githubsearchapp.UI.Detail
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.githubsearchapp.Api.RetrofitClient
 import com.example.githubsearchapp.Data.Model.User
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class FollowersViewModel : ViewModel() {
     val listFollower = MutableLiveData<ArrayList<User>>()
 
+    fun setListUser(username : String){
+        RetrofitClient.apiInstance
+            .getFollowers(username)
+            .enqueue(object : Callback<ArrayList<User>>{
+                override fun onResponse(
+                    call: Call<ArrayList<User>>,
+                    response: Response<ArrayList<User>>
+                ) {
+                    if (response.isSuccessful){
+                        listFollower.postValue(response.body())
+                    }
+                }
+
+                override fun onFailure(call: Call<ArrayList<User>>, t: Throwable) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+
+    }
+    fun getListFollower(): LiveData<ArrayList<User>>{
+        return listFollower
+    }
 }
